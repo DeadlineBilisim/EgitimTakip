d8c6dbbd13b3e90cec420cb35338f4899c11b26d
Emre Ozcelikel <emre@ozcelikel.com>
2024-05-26 20:04:47 +0300

    .

---
+++ EgitimTakip.Web/Controllers/TrainingSubjectController.cs
@@ -0,0 +1,54 @@
+﻿using EgitimTakip.Data;
+using EgitimTakip.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EgitimTakip.Web.Controllers
+{
+    public class TrainingSubjectController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TrainingSubjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            var result = _context.TrainingSubjects.ToList();
+            return Json(new { data = result });
+        }
+        [HttpPost]
+        public IActionResult Add(TrainingSubject trainingSubject)
+        {
+            _context.TrainingSubjects.Add(trainingSubject);
+            _context.SaveChanges();
+            return Ok(trainingSubject);
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            //SOFT DELETE
+            TrainingSubject trainingSubject=_context.TrainingSubjects.Find(id);
+            trainingSubject.IsDeleted = true;
+            _context.TrainingSubjects.Update(trainingSubject);
+            _context.SaveChanges();
+            return Ok(trainingSubject);
+        }
+        [HttpPost]
+        public IActionResult Update(TrainingSubject trainingSubject)
+        {
+            _context.TrainingSubjects.Update(trainingSubject);
+            _context.SaveChanges();
+            return Ok(trainingSubject);
+
+        }
+    }
+}
--- EgitimTakip.Web/Controllers/UserController.cs
+++ EgitimTakip.Web/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using EgitimTakip.Data;
 using EgitimTakip.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
@@ -28,17 +30,67 @@ namespace EgitimTakip.Web.Controllers
         public async Task<IActionResult> Login(AppUser user)
         {
             AppUser appUser = _context.Users.FirstOrDefault(u => u.UserName == user.UserName && u.Password == user.Password);
-            if ((appUser!=null))
+            if ((appUser != null))
             {
                 List<Claim> claims = new List<Claim>();
-                claims.Add(new Claim(ClaimTypes.NameIdentifier,appUser.Id.ToString()));
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, appUser.Id.ToString()));
                 claims.Add(new Claim(ClaimTypes.GivenName, appUser.UserName));
 
                 claims.Add(new Claim(ClaimTypes.Role, appUser.IsAdmin ? "Admin" : "User"));
-                
+
+                ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignInAsync(new ClaimsPrincipal(identity));
+
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
             }
 
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(AppUser user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult Update(AppUser user)
+        {
+            _context.Users.Update(user);
+            _context.SaveChanges();
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            //SOFT DELETE
+            AppUser user = _context.Users.Find(id);
+            user.IsDeleted = true;
+       
+            _context.Users.Update(user);
+
+            _context.SaveChanges();
             return Ok();
+
+        }
+
+        public IActionResult GetAll()
+        {
+            var result = _context.Users.Where(u => u.IsDeleted == false).ToList();
+
+            return Json(new { data = result });
+
         }
     }
 }
