3359bf9ca3d63ea8296d2718dff41e5615aa813c
Emre Ozcelikel <emre@ozcelikel.com>
2024-06-01 17:34:51 +0300

    GetAll yazıldı

---
+++ EgitimTakip.Web/Controllers/TrainingCategoryController.cs
@@ -0,0 +1,25 @@
+﻿using EgitimTakip.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EgitimTakip.Web.Controllers
+{
+    public class TrainingCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TrainingCategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _context.TrainingCategories.Where(tc => !tc.IsDeleted).ToList() });
+        }
+    }
+}
