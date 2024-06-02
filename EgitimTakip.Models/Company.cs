80a203f65784077391148caa8a87e9aeb5b29a83
Emre Ozcelikel <emre@ozcelikel.com>
2024-06-02 19:54:47 +0300

    Company içerisinde Users ilişkisi eklendi

--- EgitimTakip.Models/Company.cs
+++ EgitimTakip.Models/Company.cs
@@ -12,6 +12,9 @@ namespace EgitimTakip.Models
        
         public virtual ICollection<AppUser> Users { get; set; }   
         public virtual ICollection<Employee> Employees { get; set; }=new List<Employee>();
+       // public virtual ICollection<Submission> Submissions { get; set; } = new List<Submission>();
+
+   
 
     }
 }
